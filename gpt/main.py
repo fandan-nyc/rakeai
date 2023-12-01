ac49f933acafe9c2d3c8206137f3d47b9f20a78d
dfan <fandan.nyc@gmail.com>
2023-11-30 18:57:05 -0800

    debug

--- gpt/main.py
+++ gpt/main.py
@@ -4,7 +4,12 @@ from docx_manager import docx_manager
 import util
 
 if __name__ == "__main__":
+    print("start")
     gpt_cli = academic_gpt()
+    print(gpt_cli.get_destination_path())
+    print(gpt_cli.get_prompt_path())
+    print(gpt_cli.get_source_path())
+
     gpt_client = gpt_client()
     docx_manager = docx_manager(gpt_cli.get_source_path(), gpt_cli.get_destination_path())
     docx_manager.fine_tune(gpt_client, gpt_cli.get_prompt_path())
